b0855dbc02a6ca74a24ab64310a308a9320c2ac7
Colter-Hammer <therondowdle7@gmail.com>
2018-10-18 12:20:48 -0600

    Fix all of the egregious semicolon errors

--- src/pages.js
+++ src/pages.js
@@ -1,79 +1,79 @@
-import _nav_      from './templates/nav.ejs'
-import _sidebar_  from './templates/sidebar.ejs'
-import _error_    from './templates/error.ejs'
-import _chart_    from './pages/chart.ejs'
-import _settings_ from './pages/settings.ejs'
-import _projects_ from './pages/projects.ejs'
+import _nav_ from './templates/nav.ejs';
+import _sidebar_ from './templates/sidebar.ejs';
+import _error_ from './templates/error.ejs';
+import _chart_ from './pages/chart.ejs';
+import _settings_ from './pages/settings.ejs';
+import _projects_ from './pages/projects.ejs';
 
-import chartEventHandlers    from './pages/chart'
-import projectsEventHandlers from './pages/projects'
-import settingsEventHandlers from './pages/settings'
-import loginEventHandlers    from './pages/login'
+import chartEventHandlers from './pages/chart';
+import projectsEventHandlers from './pages/projects';
+import settingsEventHandlers from './pages/settings';
+import loginEventHandlers from './pages/login';
 
-import * as database         from './scripts/database'
+import * as database from './scripts/database';
 
-let loaded = false
-let onload
-window.addEventListener('load',() => {
-  loaded = true
-  onload && onload()
-})
+let loaded = false;
+let onload;
+window.addEventListener('load', () => {
+    loaded = true;
+    onload && onload();
+});
 
-export function timetracking(data){
-  if(!loaded){ return onload = () => timetracking(data) }
-  var html = _nav_({ includeSettingsButton: true });
-  html += _sidebar_({
-    page:'timetracking', 
-    _main_:_chart_,
-    data:Object.assign({isPersonal:true,database:database},data)
-  })
-  document.body.innerHTML = html
-  chartEventHandlers()
+export function timetracking(data) {
+    if (!loaded) { return onload = () => timetracking(data); }
+    var html = _nav_({ includeSettingsButton: true });
+    html += _sidebar_({
+        page: 'timetracking',
+        _main_: _chart_,
+        data: Object.assign({ isPersonal: true, database: database }, data)
+    });
+    document.body.innerHTML = html;
+    chartEventHandlers();
 }
 
-export function team(data){
-  if(!loaded){ return onload = () => team(data) }
-  var html = _nav_({ includeSettingsButton: true });
-  html += _sidebar_({
-    page:'team',
-    _main_:_chart_,
-    data:Object.assign({isPersonal:false},data)
-  })
-  document.body.innerHTML = html
-  chartEventHandlers()
+export function team(data) {
+    if (!loaded) { return onload = () => team(data); }
+    var html = _nav_({ includeSettingsButton: true });
+    html += _sidebar_({
+        page: 'team',
+        _main_: _chart_,
+        data: Object.assign({ isPersonal: false }, data)
+    });
+    document.body.innerHTML = html;
+    chartEventHandlers();
 }
 
-export function settings(data){
-  if(!loaded){ return onload = () => settings(data) }
-  var html = _nav_({ includeSettingsButton: false });
-  html += _settings_(data)
-  document.body.innerHTML = html
-  settingsEventHandlers()
+export function settings(data) {
+    if (!loaded) { return onload = () => settings(data); }
+    var html = _nav_({ includeSettingsButton: false });
+    html += _settings_(data);
+    document.body.innerHTML = html;
+    settingsEventHandlers();
 }
 
-export function projects(data){
-  if(!loaded){ return onload = () => projects(data) }
-  var html = _nav_({ includeSettingsButton: true });
-  html += _sidebar_({
-    page:'projects',
-    _main_:_projects_,
-    data:data
-  })
-  document.body.innerHTML = html
-  projectsEventHandlers()
+export function projects(data) {
+    if (!loaded) { return onload = () => projects(data); }
+    var html = _nav_({ includeSettingsButton: true });
+    html += _sidebar_({
+        page: 'projects',
+        _main_: _projects_,
+        data: data
+    });
+    document.body.innerHTML = html;
+    projectsEventHandlers();
 }
 
-export function login(){
-  if(!loaded){ return onload = () => login() }
-  var html = _nav_({ includeSettingsButton: false })
-  html += '<div class="pt-6" id="firebaseui-auth-container"></div>'
-  document.body.innerHTML = html
-  loginEventHandlers()
+export function login() {
+    if (!loaded) { return onload = () => login(); }
+    var html = _nav_({ includeSettingsButton: false });
+    html += '<div class="pt-6" id="firebaseui-auth-container"></div>';
+    document.body.innerHTML = html;
+    loginEventHandlers();
 }
 
-export function error(err){
-  if(!loaded){ return onload = () => error(err) }
-  var html = _nav_({ includeSettingsButton: false })
-  html += _error_(err)
-  document.body.innerHTML = html
+export function error(err) {
+    if (!loaded) { return onload = () => error(err); }
+    var html = _nav_({ includeSettingsButton: false });
+    html += _error_(err);
+    document.body.innerHTML = html;
 }
--- src/pages/chart.js
+++ src/pages/chart.js
@@ -1,100 +1,111 @@
-import * as time from '../scripts/time'
-import * as toggl from '../scripts/toggl'
-import * as database from '../scripts/database'
-import * as draw from '../draw/schedule'
+import * as time from '../scripts/time';
+import * as toggl from '../scripts/toggl';
+import * as database from '../scripts/database';
+import * as draw from '../draw/schedule';
 
-const sels = ['.time-button.forwards', '.time-button.backwards', 'button[data-timeunit="week"]', 'button[data-timeunit="month"]', '.chart-title']
-let forward, backward, week, month, title, dropdown
+const sels = ['.time-button.forwards', '.time-button.backwards', 'button[data-timeunit="week"]', 'button[data-timeunit="month"]', '.chart-title'];
+let forward, backward, week, month, title, dropdown, currentuser;
 
 function setDFC(dist) {
     if (isNaN(dist)) {
-        return console.error('Not a Number', dist)
+        return console.error('Not a Number', dist);
     }
     if (dist == '0') {
-        forward.classList.add('disabled')
+        forward.classList.add('disabled');
     } else {
-        forward.classList.remove('disabled')
+        forward.classList.remove('disabled');
     }
-    localStorage.distanceFromCurrent = dist
+    localStorage.distanceFromCurrent = dist;
 }
 function getDFC() {
     if (isNaN(localStorage.distanceFromCurrent)) {
-        setDFC('0')
-        return 0
+        setDFC('0');
+        return 0;
     } else {
-        return Number(localStorage.distanceFromCurrent)
+        return Number(localStorage.distanceFromCurrent);
     }
 }
 
 function loadChart() {
     // Set the page title
-    title.innerText = time.chartTitle()
+    title.innerText = time.chartTitle();
     // if not set then true
-    var isWeek = (localStorage.timeunit || 'week') == 'week'
-    var period = time.getPeriod()
+    var isWeek = (localStorage.timeunit || 'week') == 'week';
+    var period = time.getPeriod();
 
-    draw.setTimeFrame(period, isWeek)
+    draw.setTimeFrame(period, isWeek);
 
     // retrieve the data from toggl
-    // TODO: change which user it is getting based on dropdown
-    toggl.report(period, database.user, entries => {
-        /* callback is called multiple times for each pagination */
-        draw.setEntries(entries)
-    })
+    // TODO: try catch 
+    try {
+        toggl.report(period, currentuser, entries => {
+            /* callback is called multiple times for each pagination */
+            draw.setEntries(entries);
+        });
+    }
+    catch (err) {
+        d3.select('svg')
+            .innerHTML(
+                `<div class="blankslate d-flex flex-justify-center">
+                    <div class="flex-self-center">
+                        <h3>Missing API Token</h3>
+                        <p>Please go to settings and enter your Toggl API Token</p>
+                    </div>
+                </div>`
+            );
+    }
 }
 
 export default function () {
 
-    [forward, backward, week, month, title] = sels.map(s => document.querySelector(s))
+    [forward, backward, week, month, title] = sels.map(s => document.querySelector(s));
 
-    setDFC(getDFC())
+    setDFC(getDFC());
 
-    draw.initialize(document.querySelector('.svg-container'))
-    loadChart()
+    draw.initialize(document.querySelector('.svg-container'));
+    currentuser = database.user;
+    loadChart();
 
     backward.addEventListener('click', () => {
-        if (backward.classList.contains('disabled')) { return }
-        setDFC(getDFC() + 1)
-        loadChart()
-    })
+        if (backward.classList.contains('disabled')) { return; }
+        setDFC(getDFC() + 1);
+        loadChart();
+    });
 
     forward.addEventListener('click', () => {
-        if (forward.classList.contains('disabled')) { return }
-        setDFC(getDFC() - 1)
-        loadChart()
-    })
+        if (forward.classList.contains('disabled')) { return; }
+        setDFC(getDFC() - 1);
+        loadChart();
+    });
 
     week.addEventListener('click', () => {
-        if ((localStorage.timeunit || 'week') == 'week') { return }
-        setDFC('0')
-        localStorage.timeunit = 'week'
-        week.classList.add('selected')
-        month.classList.remove('selected')
-        loadChart()
-    })
+        if ((localStorage.timeunit || 'week') == 'week') { return; }
+        setDFC('0');
+        localStorage.timeunit = 'week';
+        week.classList.add('selected');
+        month.classList.remove('selected');
+        loadChart();
+    });
 
     month.addEventListener('click', () => {
-        if (localStorage.timeunit == 'month') { return }
-        setDFC('0')
-        localStorage.timeunit = 'month'
-        month.classList.add('selected')
-        week.classList.remove('selected')
-        loadChart()
-    })
+        if (localStorage.timeunit == 'month') { return; }
+        setDFC('0');
+        localStorage.timeunit = 'month';
+        month.classList.add('selected');
+        week.classList.remove('selected');
+        loadChart();
+    });
 
-    dropdown = document.querySelectorAll('#users-dropdown a')
+    dropdown = document.querySelectorAll('#users-dropdown a');
 
     // TODO: Add listener to change user dropdown
     //FIXME: Doesn't remember the user between pages
     Array.from(dropdown).forEach(dropdownItem => {
         dropdownItem.addEventListener('click', () => {
-            var period = time.getPeriod()
-            toggl.report(period, database.users[dropdownItem.getAttribute('value')], entries => {
 
-                /* callback is called multiple times for each pagination */
-                draw.setEntries(entries)
-            })
-        })
+            currentuser = database.users[dropdownItem.getAttribute('value')];
+            loadChart();
+        });
 
-    })
+    });
 }
